6f5daad876f44b1f668f0eccba626467a0ec3886
Qing Huang <117525827+SE-qinghuang@users.noreply.github.com>
2023-02-25 19:16:36 +0800

    Delete IDE Plugin/src directory

--- IDE Plugin/src/main/java/com/example/AlxTyper/data/Datasort.java
+++
@@ -1,9 +0,0 @@
-package com.example.AlxTyper.data;
-
-public class Datasort {
-    public static String method_split(Notedata notedata){
-         String partial_code = notedata.getContent();
-         String new_partial_code = partial_code+"Hello_Java";
-         return new_partial_code;
-    }
-}
